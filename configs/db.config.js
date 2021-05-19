7c4921302b0ada3f79a2f55e430fea1b5d7bda22
antonicasasayasg <antoni.casasayas1@gmail.com>
2021-05-19 09:34:43 +0200

    deploy

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`${process.env.MONGODB_URI}`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`${process.env.DBURL}`, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Connected to Mongo!'))
 .catch((error) => console.error(error))
 
